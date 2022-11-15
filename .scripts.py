0978b9dad7663efad973d6c2b5cf22503b173b6c
luissilva1044894 <26727089+luissilva1044894@users.noreply.github.com>
2022-11-15 09:26:40 -0300

    Add regex

--- .scripts.py
+++ .scripts.py
@@ -6,10 +6,13 @@ from os.path import (
   join,
   isfile,
 )
+import re
 
 GAMES = [
 ]
 EXC = ''
+DEV_ID_RE = re.compile(r'^[a-zA-Z]{2}[0-9]{3}\-?[a-zA-Z]{1}[0-9]{1}$')
+TITLE_ID_RE = re.compile(r'^(?P<type>[a-zA-Z]{4})\_?(?P<id>[0-9]{3}\.?[0-9]{2})(?P<release>[a-zA-Z]{1})*$')
 ICONS_FOLDER = join(abspath(dirname(__file__)), 'ICNS')
 with open('./Icons.ini', 'r') as f:
   for line in f.readlines():
@@ -23,7 +26,10 @@ with open('./Icons.ini', 'r') as f:
       EXC += f'Wrong symbol: {line}\r\n'
     else:
       game, ico = line.split('=', 1)
-      if game in GAMES:
+      #Ignore weird id
+      if 'ARP2_012.01' not in game and TITLE_ID_RE.match(game) is None and DEV_ID_RE.match(game) is None:
+        EXC += f'Wrong Title ID: {line}\r\n'
+      elif game in GAMES:
         EXC += f'Already listed: {line}\r\n'
       else:
         ico_path = join(ICONS_FOLDER, ico.replace('\n', ''))
